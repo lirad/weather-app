17ddaf99bfae3d0a8356313737b67b1aee547147
Diego Lira <diegoalira@gmail.com>
2021-01-27 00:16:32 -0300

    Finishing styling

--- src/api/weather.js
+++ src/api/weather.js
@@ -8,13 +8,11 @@ const api = axios.create({
 });
 
 const currentWeather = async (city) => {
-
   const data = await api.get(`weather?q=${city}&units=metric&appid=a24f6738ba9b0611a52d8603e12042f0`)
     .then(res => res.data)
     .catch(err => console.error(err));
-
-
   return data;
 };
+
 
 export { currentWeather };
--- src/interface/result.js
+++ src/interface/result.js
@@ -1,12 +1,36 @@
+
 import loading from '../components/loading';
 
 const Result = (data) => {
   const html = `
-  <div class="col-span-10 mx-auto bg-white rounded-lg opacity-50 mt-10 w-full h-56 flex align-middle justify-center">
+  <div class="col-span-10 mx-auto bg-white rounded-lg  mt-10 w-full h-56 ">
     <div class="">   
-      <div class="text-3xl font-mono p-5">
-      <i class="wi wi-cloudy"></i>
-      ${data.main && `${data.main.temp}°C`}  
+      <div class="font-mono p-5 flex justify-around">
+        <div class="text-center flex flex-col">
+          
+          <img  class="object-contain" src="http://openweathermap.org/img/wn/${data.main && data.weather[0].icon}@2x.png" /></span
+          <span class="text-indigo-700"> ${data.main && data.weather[0].main}</span>
+          <span> ${data.main && data.weather[0].description}</span>
+        </div>
+        <div class="flex flex-col justify-center">
+          <div class="flex-col">
+            <div class="text-2xl">
+              ${data.main && `${data.main.temp.toFixed(0)}°C`}  
+              <span> Temperature</span>
+            </div>
+            <div class="flex justify-between">
+              <div class="text-red-700">
+                ${data.main && `${data.main.temp_max.toFixed(0)}°C Max`}  
+              </div>
+              <div class="text-blue-700">
+                ${data.main && `${data.main.temp_min.toFixed(0)}°C Min`}  
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      
+      
       </div>
     </div>
   </div>
